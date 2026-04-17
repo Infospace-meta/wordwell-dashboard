693917d94a1e4488a14575333204455b1d66e475
joe052 <joekaranjasenior52@gmail.com>
2026-04-17 13:26:05 +0300

    message

--- app/src/_nav.js
+++ app/src/_nav.js
@@ -1,86 +1,23 @@
 export default [
   {
-    component: 'CNavItem',
-    name: 'Dashboard',
-    to: '/dashboard',
-    icon: 'cil-speedometer',
+    component: "CNavItem",
+    name: "Dashboard",
+    to: "/dashboard",
+    icon: "cil-speedometer",
     badge: {
-      color: 'primary',
+      color: "primary",
       // text: 'NEW',
     },
   },
 
-    {
-    component: 'CNavItem',
-    name: 'Orders',
-    to: '/orders',
-    icon: 'cil-speedometer',
+  {
+    component: "CNavItem",
+    name: "Orders",
+    to: "/orders",
+    icon: "cil-speedometer",
     badge: {
-      color: 'primary',
+      color: "primary",
       // text: 'NEW',
     },
   },
-  // {
-  //   component: 'CNavTitle',
-  //   name: 'Components',
-  // },
-  // {
-  //   component: 'CNavGroup',
-  //   name: 'Icons',
-  //   to: '/icons',
-  //   icon: 'cil-star',
-  //   items: [
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'CoreUI Icons',
-  //       to: '/icons/coreui-icons',
-  //       badge: {
-  //         color: 'info',
-  //         text: 'NEW',
-  //       },
-  //     },
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Brands',
-  //       to: '/icons/brands',
-  //     },
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Flags',
-  //       to: '/icons/flags',
-  //     },
-  //   ],
-  // },
-  // {
-  //   component: 'CNavTitle',
-  //   name: 'profile',
-  // },
-  // {
-  //   component: 'CNavGroup',
-  //   name: 'Pages',
-  //   to: '/pages',
-  //   icon: 'cil-star',
-  //   items: [
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Login',
-  //       to: '/pages/login',
-  //     },
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Register',
-  //       to: '/pages/register',
-  //     },
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Error 404',
-  //       to: '/pages/404',
-  //     },
-  //     {
-  //       component: 'CNavItem',
-  //       name: 'Error 500',
-  //       to: '/pages/500',
-  //     },
-  //   ],
-  // },
-]
+];
